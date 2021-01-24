511150d41c1924f3cd3b678843b062ba1adc6cf7
minhnguyen303 <minh09az09@gmail.com>
2021-01-24 17:47:41 +0700

    Add search function

--- src/View/products.php
+++ src/View/products.php
@@ -3,14 +3,27 @@ $products = null;
 if (!empty($this->productManager)) {
     $products = $this->productManager->getAllProduct();
 }
+if($_SERVER['REQUEST_METHOD']=='GET' && isset($_GET['search'])){
+
+    $productSearch = [];
+
+    $nameSearch = $_GET['search'];
+
+    foreach ($products as $product) {
+        if (substr($product->getName(), 0, strlen($_GET['search'])) == $_GET['search']){
+            $productSearch[] = $product;
+        }
+    }
+    $products = $productSearch;
+}
 ?>
 <div class="container mt-5">
     <table class="table table-striped table-bordered caption-top">
         <caption>
             <h1>Danh sách mặt hàng</h1>
             <div class="pt-2 pb-3 d-flex justify-content-between">
-                <form class="d-flex">
-                    <input class="form-control me-2" type="text" placeholder="Search" aria-label="Search">
+                <form class="d-flex" method="get">
+                    <input class="form-control me-2" type="text" name="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-primary" type="submit">Search</button>
                 </form>
                 <a class="btn btn-outline-success pb-2" href="index.php?page=create-product" role="button">Create</a>
@@ -25,6 +38,11 @@ if (!empty($this->productManager)) {
         </tr>
         </thead>
         <tbody>
+        <?php if(empty($listProduct)):?>
+            <tr>
+                <th scope="row" colspan="4" class="text-center">No result is found</th>
+            </tr>
+        <?php endif;?>
         <?php foreach ($products as $product):?>
         <?php $id = $product->getId(); $_SESSION["$id"] = $product;?>
             <tr>
